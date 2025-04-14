60247c5ff43ff72eb315d716edf6e1c6c570c096
Ethan Crowson <ethancrowson@hotmail.co.uk>
2025-04-14 17:56:37 +0100

    Task controller made with calls to the service methods.

--- src/main/java/com/ecrowson/hmctsdevchallenge/controller/TaskController.java
+++ src/main/java/com/ecrowson/hmctsdevchallenge/controller/TaskController.java
@@ -1,5 +1,45 @@
 package com.ecrowson.hmctsdevchallenge.controller;
 
+import com.ecrowson.hmctsdevchallenge.model.Status;
+import com.ecrowson.hmctsdevchallenge.model.Task;
+import com.ecrowson.hmctsdevchallenge.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class TaskController {
 
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@Valid @RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTask(@PathVariable Long id) {
+        return ResponseEntity.ok(taskService.getTaskById(id));
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTaskStatus(@PathVariable Long id, @Valid @RequestBody Status status) {
+        return ResponseEntity.ok(taskService.updateTaskStatus(id, status));
+    }
+
+    @DeleteMapping("(/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTaskById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
