23ae2d75fab8c8d648e70f7bcb7d30ec2d482566
Ethan Crowson <ethancrowson@hotmail.co.uk>
2025-04-14 11:52:05 +0100

    Initial Commit

---
+++ src/main/java/com/ecrowson/hmctsdevchallenge/HmctsDevChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.ecrowson.hmctsdevchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmctsDevChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmctsDevChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecrowson/hmctsdevchallenge/HmctsDevChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecrowson.hmctsdevchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmctsDevChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
